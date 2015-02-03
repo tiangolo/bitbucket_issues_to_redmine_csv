66efc7f1a04934cb62e34399e9b67fe8507dfc29
Sebastián Ramírez <tiangolo@gmail.com>
2015-02-03 18:22:51 -0500

    Fix description in bitbucket_issues_to_redmine_csv.py

--- bitbucket_issues_to_redmine_csv.py
+++ bitbucket_issues_to_redmine_csv.py
@@ -6,6 +6,14 @@ Export that data to a CSV file formatted according to the plug-in Redmine Import
 https://github.com/leovitch/redmine_importer/wiki
 
 Use the latest version of the plug-in from: https://github.com/zh/redmine_importer
+
+If the names of the users in Bitbucket are different from the names of the users in Redmine, it can use a mapping file:
+a CSV file with one column with the username from Bitbucket and another column with the username from Redmine, for
+example:
+::
+     username, user.name
+     anotheruser, redmine_another_user
+     sameusername, sameusername
 """
 
 import json
